5e861e63ad3e35aece5bbc9f02834b59c50f227b
Zabuka_zuzu <zuzu950444@gmail.com>
2023-03-07 15:21:47 +0000

    mist: Welcome to MistOS

--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -1,9 +1,9 @@
 #
 # This policy configuration will be used by select legacy products that
-# inherit from Lineage
+# inherit from Mist
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/cherish/sepolicy/legacy-common/public/legacy-camera-hal1
+    device/mist/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
